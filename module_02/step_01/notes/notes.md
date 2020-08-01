496fab197d7fde1abfdab9e8e1afbf9e9754df99
Yann Depps <yann.deperrot@protonmail.com>
2020-08-01 15:08:56 +0200

    short-circuiting

--- module_02/step_01/notes/notes.md
+++ module_02/step_01/notes/notes.md
@@ -2,7 +2,7 @@
 
 ### Types & Conditionals
 
-#### conditionnaly run JS code
+#### Conditionnaly run JS code
 
 - applications need the ability to do things based on different conditions
 - there are two ways of modifying flow control to achieve this
@@ -37,7 +37,7 @@
   - N/B : in switch statements, if none of the cases match, the default case runs
 
 
-### types and how they can be changed
+### Types and how they can be changed
 
 - there are two ways to convert data from one type to another in JS :
   1. **explicit** type conversion : where we tell JS that we want to change 
@@ -58,3 +58,38 @@
   1. avoid direct comparisons in conditionals
   2. use triple equals ( strict equals operator ) to prevent type conversion
   3. convert to real Boolean values where needed
+
+
+### Shorten conditionals with ternaries
+
+- if/else statements can be repetitive
+- in order to conditionnaly assign a variable without repetition,
+  we can use **ternary operators** :
+  ```
+  const cartItemCount = isAuthenticated ? 1 : 0;
+  ```
+  
+- N/B : continue to use if/else statements and not ternaries :
+  1. if you want to perform an action
+  2. if the condition is complex
+
+- the ternary operator is an expression not a statement
+
+
+### Even Shorter Conditionals with Short-Circuiting
+- we can make conditionals shorter with **logical operators** such
+  as || ( or ) or && ( and ) 
+- this is known as **short-circuiting**
+  ```
+  const username = response || "guest";
+  const username = isEmailVerified && response || "guest";
+  ```
+  
+- N/B : **operator precedence** ( the order in which the operations are performed )
+  means that && will be performed before ||
+  if we need to change this, we can use parentheses ( || ) which have the
+  highest operator precedence of all
+  ```
+  const username = isEmailVerified && (response || "guest");
+  ```
+
--- module_02/step_01/src/app.js
+++ module_02/step_01/src/app.js
@@ -48,7 +48,7 @@
 //     console.log('hello');
 // }
 
-// types and how they can be changed
+// Types and how they can be changed
 /*
  * primitive types :
  *
@@ -77,22 +77,157 @@
 // console.log(coer);
 // console.log(typeof coer);
 
-// avoid direct comparisons in conditionals :
+// Avoid direct comparisons in conditionals :
 // const userName = '';
 // if (!userName) {
 //   console.log('no user');
 // }
 
-// use triple equals ( strict equals operator ) : it will prevent coercion
+// Use triple equals ( strict equals operator ) : it will prevent coercion
 // if (null === undefined) {
 //   console.log('equals');
 // } else {
 //   console.log('not equals');
 // }
 
-// convert to real Boolean values when needed :
-if (Boolean(NaN) === Boolean(NaN)) {
-  console.log('equals');
-} else {
-  console.log('not equals');
-}
+// Convert to real Boolean values when needed :
+// if (Boolean(NaN) === Boolean(NaN)) {
+//   console.log('equals');
+// } else {
+//   console.log('not equals');
+// }
+
+// Challenge 1:
+// What will the following console logs display? (they're all true and false)
+
+// console.log("Challenge 1:");
+// console.log(!undefined); // true
+// console.log(Boolean(NaN)); // false
+// console.log(false === false); // true
+// console.log(5 === "5"); // false
+// console.log("Hello" == "hello"); // false
+
+// Challenge 2:
+// What will the following console logs display? (they're all true and false)
+// console.log("Challenge 2:");
+// console.log(Boolean(0)); // false
+// console.log(Boolean("0")); // true
+// console.log(Boolean("")); // false
+// console.log(!null); // true
+// console.log(!!"hello"); // true
+
+// Challenge 3:
+// List all the falsy values in JavaScript
+// NaN, undefined, 0, "", null, false
+
+// shorten conditionals with ternaries :
+// const isAuthenticated = true;
+// let cartItemCount = 0;
+
+// if (isAuthenticated) {
+//   // add item to cart
+//   cartItemCount = 1;
+//   console.log('cart:', cartItemCount);
+// } else {
+//   // tell user to log in
+//   cartItemCount = 0;
+//   // console.log('please log in!');
+//   console.log('cart:', cartItemCount);
+// }
+
+// const isAuthenticated = true;
+// const cartItemCount = isAuthenticated ? 1 : 0;
+// console.log(cartItemCount);
+
+// const age = 20;
+// let greeting;
+
+// if (age < 10) {
+//   greeting = 'hey there!';
+// } else {
+//   greeting = 'interesting age!';
+// }
+
+// console.log(greeting);
+
+// const age = 20;
+// const greeting = age < 10 ? 'hey there!' : 'interesting age!';
+// console.log(greeting);
+
+// Chain multiple ternaries together :
+
+// const age = 15;
+// let greeting;
+
+// if (age < 10) {
+//   greeting = 'hey there!';
+// } else if (age > 18) {
+//   greeting = 'Greetings!'
+// } else if (age > 15) {
+//   greeting = 'whats up!'
+// } else {
+//   greeting = 'interesting age!';
+// }
+
+// console.log(greeting);
+
+// const age = 15;
+// const greeting = age < 10 ? "Hey there" : age > 18 ? "Greetings" : age > 10 ? "What's up?" : "That's an interesting age!";
+// console.log(greeting);
+
+// Challenge 1: Check if the user is allowed to add an "iPad" to its shopping cart. Write a ternary that sets the shoppingCart variable to "iPad" if the user is authenticated, and an empty string if the user isn't authenticated
+
+// const isAuthenticated = true;
+// let shoppingCart;
+
+// write your code here
+// const isAuthenticated = true;
+// const shoppingCart = isAuthenticated ? 'iPad' : 'no user';
+// console.log("shoppingCart: ", shoppingCart);
+
+
+// Challenge 2: Check if user is old enough. Write a ternary that sets allowedAccess to true if the age is 18 or more, or false if not
+// const age = 20;
+// let allowedAccess;
+
+// write your code here
+// const age = 18;
+// const allowedAccess = age >= 18 ? 1 : 0;
+
+// console.log("allowedAccess: ", allowedAccess);
+
+// Even Shorter Conditionals with Short-Circuiting
+// const response = 'yann';
+// let userName;
+
+// if (response) {
+//   userName = response;
+// } else {
+//   userName = 'guest';
+// }
+
+// const response = 'yann';
+// const userName = response ? response : 'guest';
+
+// console.log(userName);
+
+// 1. using || ( OR operator ) -> return the truthy valuev:
+// if we have a value, use it. otherwise use the fallback
+// const response = 'yann';
+// const userName = response || 'guest';
+// console.log(userName);
+
+// 2. evalute multiple conditions with && ( AND operator ) -> return the falsy value
+const response = 'yann';
+const isEmailVerified = true;
+
+// const userName = response && isEmailVerified || 'guest';
+// one value being falsy, returns fallback. take care of the order of operation with && !
+// be careful of operator precedence : the order in which the operation are performed
+// && as higher order precedence over || for instance. we can use () to overwrite
+// operator precedence :
+const userName = isEmailVerified && (response || 'guest');
+// that's because () have the highest order in JS
+// so if we want one expression to be evaluated first, we wrap it into ()
+// const userName = isEmailVerified && response || 'guest';
+// console.log(userName);
